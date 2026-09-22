982e9352ee6f725747108bfd5ef4e0695dc32e53
MuhammadZainButt7899 <zainbutt03311462196@gmail.com>
2026-09-22 13:14:07 -0700

    Add leap year and passing system practice

--- script.js
+++ script.js
@@ -45,3 +45,57 @@ function numberChecking() {
         "Your Number is Negative ."
     }
 }
+
+// grade calculator
+function calculateGrade() {
+    let score = document.getElementById("score").value;
+    let gradeResult = document.getElementById("gradeResult");
+
+    if (score === "") {
+        gradeResult.textContent = "Please enter your score.";
+    }else if (score < 0 || score > 100) {
+        gradeResult.textContent = "Please enter a valid score between 0 and 100.";
+    }else if (score >= 90) {
+        gradeResult.textContent = "Your grade is A.";
+    } else if (score >= 80) {
+        gradeResult.textContent = "Your grade is B.";
+    } else if (score >= 70) {
+        gradeResult.textContent = "Your grade is C.";
+    } else {
+        gradeResult.textContent = "Your grade is D.";
+    }
+}
+
+// leap year checking
+function checkLeapYear() { 
+    let year = document.getElementById("year").value;
+     let leapYearResult = document.getElementById("leapYearResult");
+     
+     if (year === "") {
+         leapYearResult.textContent = "Please enter a year.";
+         } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) { 
+            leapYearResult.textContent = "This is a leap year."; 
+        } else { 
+            leapYearResult.textContent = "This is not a leap year.";
+         } 
+        }
+
+
+// checkFailPass
+
+function checkFailPass() {
+    let marks = document.getElementById("marks").value;
+    let failPassResult = document.getElementById("failPassResult"); 
+    
+    if (marks === "") {
+        failPassResult.textContent = "Please enter your marks.";
+    }else if (marks < 0 || marks > 100) {
+        failPassResult.textContent = "Please enter valid marks between 0 and 100.";
+    } else {
+        if (marks >= 50) {
+            failPassResult.textContent = "You have passed the exam.";
+        } else {
+            failPassResult.textContent = "You have failed the exam.";
+        }
+    }
+}
