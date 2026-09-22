a0e65781047623df1d8624524d979c8b88764b95
MuhammadZainButt7899 <zainbutt03311462196@gmail.com>
2026-09-22 12:10:39 -0700

    Add more if-else practice

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
     }
     else if (age >= 18){
         document.getElementById("result").textContent = "You are eligible to vote.";
-    }  
+    } 
     else{
         document.getElementById("result").textContent = "You are not eligible to vote.";
     }
@@ -28,3 +28,19 @@ function checkEvenOdd() {
         document.getElementById("evenOddResult").textContent = "The number is odd.";
     }
 }   
+// numberChecking
+function numberChecking(){
+    let num = document.getElementById("num").value;
+    if(num == 0){
+        document.getElementById("pos-neg-resul").textContent =
+        "Your Number is equal to Zero ."
+    }
+    else if (num > 0){
+         document.getElementById("pos-neg-resul").textContent =
+        "Your Number is Positive ."
+    }
+    else{
+        document.getElementById("pos-neg-resul").textContent =
+        "Your Number is Negative ."
+    }
+}
